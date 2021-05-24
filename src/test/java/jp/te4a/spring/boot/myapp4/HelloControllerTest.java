ab1c2722fa462517780165e48d4fe5bc33ff5326
te180453 <te180453jc21@gmail.com>
2021-05-24 04:32:01 +0000

    test and add badge

--- src/test/java/jp/te4a/spring/boot/myapp4/HelloControllerTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp4/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package jp.te4a.spring.boot.myapp4;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -19,7 +18,6 @@ public class HelloControllerTest
         HelloController helloController = new HelloController();
         String expected = "this is Spring Boot sample";
         String actual = helloController.index();
-        
         assertEquals(expected,actual);
     }
 }
