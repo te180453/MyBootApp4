b8fcf44797c5562a63bdbbcb0c3a1ca94e99ce67
te180453 <te180453jc21@gmail.com>
2021-05-24 09:15:14 +0000

    add model and view

--- src/main/java/jp/te4a/spring/boot/MyBootApp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/MyBootApp5/HelloController.java
@@ -3,6 +3,9 @@ package jp.te4a.spring.boot.MyBootApp5;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController{
@@ -12,5 +15,12 @@ public class HelloController{
         //return "this is Spring Boot sample";
         model.addAttribute("msg", "this is a setting message");
         return "index";
+    }
+    
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1){
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", String.format("you write %s!!!",text1));
+        return mv;
     }
 }
