17a561883ccda137f62fd92443ef074f7982ef09
te180453 <te180453jc21@gmail.com>
2021-05-24 07:35:35 +0000

    pomのjasperのバージョン消したら動いた

--- src/main/java/jp/te4a/spring/boot/MyBootApp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/MyBootApp5/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController{
     @RequestMapping("/")
     public String index(Model model){
         //return "this is Spring Boot sample";
-        model.addAttribute("msg", "this is a setting message");
+        //model.addAttribute("msg", "this is a setting message");
         return "index";
     }
 }
