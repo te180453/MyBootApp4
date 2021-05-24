4ad2a4cfacf93d72ef42c79e8d242223e26bf73b
te180453 <te180453jc21@gmail.com>
2021-05-24 01:47:45 +0000

    init

---
+++ src/main/java/jp/te4a/spring/boot/myapp4/App.java
@@ -0,0 +1,19 @@
+package jp.te4a.spring.boot.myapp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+ @SpringBootApplication
+ @EnableAutoConfiguration
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+}
